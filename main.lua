3ea04fb3d96be32076e4f6b8ced10b5a7ef9c1ba
meowgix <meowgixmailbox@gmail.com>
2022-09-11 12:16:02 -0400

    🎉 Continue experiment

---
+++ main.lua
@@ -0,0 +1,16 @@
+--function love.draw()
+--    love.graphics.print("Hello World", 400, 300)
+--end
+
+--dog = 5
+--cat = 7
+--cow = dog + cat
+--print(dog + cat)
+--dog = 10
+--cat = 5
+--print(cow)
+
+name = "ellie"
+age = "11"
+text = "hi guys, my name is " .. name .. ", and i am only " .. age .. " years old."
+print(text)
